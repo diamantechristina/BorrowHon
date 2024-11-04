24db5ee95c05a463bcf8c6b47e2ee3d0a93878d9
Beirun <orillobernardjay@gmail.com>
2024-11-04 11:07:19 +0800

    update

--- backend/routes/history.route.js
+++ backend/routes/history.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { createHistory, getHistory } from "../controller/history.controller.js";
+import { createHistory, getHistory, updateHistory, deleteHistory } from "../controller/history.controller.js";
 
 const router = express.Router();
 
 router.get('/', getHistory)
 router.post('/', createHistory)
+router.put('/:id', updateHistory)
+router.delete('/:id', deleteHistory)
 
 
 export default router
